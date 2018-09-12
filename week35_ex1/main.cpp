35456b520d7035b786098de2003d3030e2794606
Soeren Korsgaard <Soeren@vbox-Centos.ase.au.dk>
2018-09-12 13:25:32 +0200

    week35_ver1

---
+++ week35_ex1/main.cpp
@@ -0,0 +1,59 @@
+/*
+
+ * main.cpp
+ *
+ *  Created on: 23 Aug 2018
+ *      Author: Soeren
+ */
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main ( )
+{
+	char num = '0';
+
+	cout << "Goddag. Jeg er dit interaktive Hej verden program" << endl;
+
+	cout << "vælg mellem 5 citater ved at trykke 1-5 og 9 for at afslutte"
+	    << endl;
+
+	cin >> num;
+
+	while (num != 9)
+	{
+		if (num == 1)
+		{
+			cout << "Det går nok" << endl;
+		}
+		else if (num == 2)
+		{
+			cout << "det er møj fint" << endl;
+		}
+		else if (num == 3)
+		{
+			cout << "virker det ikke. brug en større hammer" << endl;
+		}
+		else if (num == 4)
+		{
+			cout << "jeg kan næsten smage weekenden" << endl;
+		}
+		else if (num == 5)
+		{
+			cout << "det bliver nemt idag og dog" << endl;
+		}
+		else
+		{
+			cout << "vælg mellem 5 citater ved at trykke 1-5 og 9 for at afslutte"
+			    << endl;
+		}
+		cin >> num;
+	}
+	cout << "Tak fordi du anvendte. Hej verden i C++" << endl;
+
+	return 0;
+}
+
