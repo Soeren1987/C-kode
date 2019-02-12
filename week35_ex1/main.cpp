3049802d774e0daac4845251614100b384aff3dd
Soeren Korsgaard <Soeren@vbox-Centos.ase.au.dk>
2019-02-12 20:16:15 +0100

    all files

--- week35_ex1/main.cpp
+++ week35_ex1/main.cpp
@@ -23,31 +23,31 @@ int main ( )
 
 	cin >> num;
 
-	while (num != 9)
+	while (num != '9')
 	{
-		if (num == 1)
+		if (num == '1')
 		{
 			cout << "Det går nok" << endl;
 		}
-		else if (num == 2)
+		else if (num == '2')
 		{
 			cout << "det er møj fint" << endl;
 		}
-		else if (num == 3)
+		else if (num == '3')
 		{
 			cout << "virker det ikke. brug en større hammer" << endl;
 		}
-		else if (num == 4)
+		else if (num == '4')
 		{
 			cout << "jeg kan næsten smage weekenden" << endl;
 		}
-		else if (num == 5)
+		else if (num == '5')
 		{
 			cout << "det bliver nemt idag og dog" << endl;
 		}
 		else
 		{
-			cout << "vælg mellem 5 citater ved at trykke 1-5 og 9 for at afslutte"
+			cout << "vælg mellem 5 citater ved at trykke 1-5 og 9 for at afslutte in loop"
 			    << endl;
 		}
 		cin >> num;
